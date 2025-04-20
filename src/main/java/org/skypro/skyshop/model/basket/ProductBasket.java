97be2b5d497c30dfd317a0321e61e945551d8f9a
Александр <klerik.af.miass@gmail.com>
2025-04-20 07:33:38 +0500

    Corrected Home-Work 2.

--- src/main/java/org/skypro/skyshop/model/basket/ProductBasket.java
+++ src/main/java/org/skypro/skyshop/model/basket/ProductBasket.java
@@ -1,6 +1,7 @@
 package org.skypro.skyshop.model.basket;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import org.springframework.web.context.annotation.SessionScope;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -8,11 +9,13 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+@SessionScope
 public class ProductBasket {
-    private final Map<UUID,Integer> products = new HashMap<>();
-public void addProduct(UUID id){
-    products.merge(id, 1, Integer::sum);
-}
+    private final Map<UUID, Integer> products = new HashMap<>();
+
+    public void addProduct(UUID id) {
+        products.merge(id, 1, Integer::sum);
+    }
 
     public Map<UUID, Integer> getProducts() {
         return Collections.unmodifiableMap(products);
